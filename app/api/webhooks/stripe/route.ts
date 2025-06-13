48491622d8fe6e19ed8c7888ea41dae7c32e1352
jokings2k <eromoselejonthan@gmail.com>
2025-06-13 18:53:14 +0100

    sign

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,6 +3,8 @@ import { stripe } from "@/lib/stripe/strpe";
 import { headers } from "next/headers";
 
 export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+export const revalidate = 0
 
 export async function POST(request: Request) {
   const body = await request.text();
